a42dc51fb35ec561932e6d7f4549a3f1d06d4339
Atifkhan-1234 <mdatifkhan17112005@gmail.com>
2024-09-09 06:57:04 +0000

    adding variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 2244553
+let accountPassword = 12345
+var accountEmail = "atif@ggoogle.com"
+accountCity = "Delhi"
+
+// accountId = 2345 // not allowed
+console.log(accountId);
+accountPassword = 764381
+accountEmail = "atif@ac.in"
+accountCity = "banguluru"
+
+console.table([accountPassword, accountEmail, accountCity]);
+
